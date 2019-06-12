cf6bca7e91864dfa72ba534671ee96394e0ef302
thehanir95 <39161188+thehanir95@users.noreply.github.com>
2019-06-12 07:16:31 +0530

    Add files via upload

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<html>
+<head><title>Arrays</title>
+</head>
+<body>
+<?php
+
+$arr=array(23,25,8,35.2,'nimal',"nice");
+echo $arr[0];
+echo '<br>';
+echo $arr[3];
+echo '<br>';
+echo $arr[4];
+echo '<br>';
+print_r($arr);
+echo '<br>';
+$myarr=[2,2.36,'kamal',[1,2,3,4]];
+echo $myarr[0][1];
+print_r($myarr);
+
+?>
+</body>
+</html>
